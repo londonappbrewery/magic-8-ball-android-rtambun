205222b161dd018c8b0a9be05325fb4aee61ec3d
Richson Untung Tambun <richson.tambun@gmail.com>
2017-10-30 16:01:17 +0800

    Task completed

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,7 +1,15 @@
 package com.londonappbrewery.magiceightball;
 
+import android.net.sip.SipAudioCall;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+import static android.R.attr.onClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button askButton = (Button)findViewById(R.id.button_ask);
+        final ImageView ballDisplay = (ImageView)findViewById(R.id.image_eightBall);
+
+        final int[] ballArray = {R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5};
+
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Random generateRandomNumber = new Random();
+                int number = generateRandomNumber.nextInt(5);
+
+                ballDisplay.setImageResource(ballArray[number]);
+            }
+        });
     }
 }
